8b1aad98e7cc2b628b6e23a2973a9f28871fbc4b
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-04-07 05:19:22 +0500

    feat: user router

--- api/apiVersions/v1.js
+++ api/apiVersions/v1.js
@@ -2,6 +2,8 @@ module.exports.prepareV1Routes = (app) => {
   const prefix = '/api/v1/';
 
   const helpersRouter = require('../routers/v1/helperRouter');
+  const userRouter = require('../routers/v1/userRouter');
 
   app.use(`${prefix}helper`, helpersRouter);
+  app.use(`${prefix}user`, userRouter);
 };
---
+++ api/routers/v1/userRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const {register, login} = require('../../utils/v1/user');
+
+const userRouter = express.Router();
+
+userRouter.use(bodyParser.json());
+
+userRouter
+  .route('/register')
+  .all((req, res, next) => {
+    res.status(200).setHeader('Content-Type', 'application/json');
+    next();
+  })
+  .post((req, res, next) => {
+    try {
+      register(req.body, res);
+    } catch (err) {
+      res.status(500).send({message: 'Internal Server Error!'});
+    }
+  });
+
+module.exports = userRouter;
