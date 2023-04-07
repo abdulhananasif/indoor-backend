7ec233c7e7b47528fa6abc9e3dbe7bd3629b3eb4
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-04-07 05:32:21 +0500

    feat: user login api

--- api/utils/v1/user.js
+++ api/utils/v1/user.js
@@ -2,6 +2,8 @@ const Joi = require('joi');
 
 const bcrypt = require('bcrypt');
 
+const _ = require('lodash');
+
 const User = require('../../../mongodb/models/user');
 
 const saltRounds = 10;
@@ -94,4 +96,62 @@ const register = async (userInfo, res) => {
   return res.status(response.status).send(response.data);
 };
 
-module.exports = {register};
+const login = async (userInfo, res) => {
+  let response = {};
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.base': 'Email must be a string!',
+      'string.email': 'Email must be a valid email!',
+      'any.required': 'Email is a required!',
+    }),
+    password: Joi.string().required().messages({
+      'string.empty': 'Password is not allowed to be empty!',
+      'any.required': 'Password is a required!',
+    }),
+  });
+  try {
+    await schema.validateAsync(userInfo);
+    const user = await User.findOne({
+      email: userInfo.email,
+    });
+    if (user) {
+      const isPasswordMatched = await bcrypt
+        .compare(userInfo.password, user.password)
+        .then((result) => {
+          return result;
+        });
+      if (isPasswordMatched) {
+        const userToSend = {};
+        userToSend.name = user.name;
+        userToSend.email = user.email;
+        userToSend.username = user.username;
+        userToSend._id = user._id;
+        response.status = 200;
+        response.data = {
+          success: true,
+          message: 'Login successful!',
+          userInfo: userToSend,
+        };
+      } else {
+        throw {message: 'Invalid password!'};
+      }
+    } else {
+      throw {message: 'User is not registered!'};
+    }
+  } catch (err) {
+    let errorMessage = '';
+    if (err.details) {
+      err.details.map((err) => (errorMessage += err.message));
+    } else {
+      errorMessage += err.message;
+    }
+    response.status = 400;
+    response.data = {
+      success: false,
+      error: errorMessage,
+    };
+  }
+  return res.status(response.status).send(response.data);
+};
+
+module.exports = {register, login};
